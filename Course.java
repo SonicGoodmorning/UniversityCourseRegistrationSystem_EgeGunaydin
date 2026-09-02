84aac73bdf8ca67e627b11ab6b6c45c84258e9c2
MrGoodmorning <egegoodmorning@gmail.com>
2026-09-02 10:12:03 -0400

    first commit

---
+++ Course.java
@@ -0,0 +1,25 @@
+public class Course{
+    String courseCode;
+    String title;
+    int credits;
+    double tuitionPerCredit;
+
+    public Course(String cc,String t, int c, double tpc){
+        this.courseCode = cc;
+        this.title = t;
+        this.credits = c;
+        this.tuitionPerCredit = tpc;
+    }
+
+    public String getCourseCode(){return courseCode;}
+    public String getTitle(){return title;}
+    public int getCredits(){return credits;}
+    public double getTuitionPerCredit(){return tuitionPerCredit;}
+    public double calculateTution(){return credits*tuitionPerCredit;}
+
+    public void displayInfo(){System.out.println(courseCode+ 
+    "\n" + title + "\n" + credits + "\n" + tuitionPerCredit);}
+
+    
+
+}
